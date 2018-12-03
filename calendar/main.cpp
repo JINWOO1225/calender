ca02a6b6dcf91808880bfab2949877042cffa9f1
JINWOO1225 <jinwoo17962@gmail.com>
2018-12-03 19:31:10 +0900

    adding daymax
    daymax함수를 통해 해당 달의 최대값을 확인할수있다.

--- calendar/main.cpp
+++ calendar/main.cpp
@@ -10,15 +10,20 @@
  여러분은 아래와 같은 Date 클래스를 디자인 하려고 합니다. set_date 는 말그대로 Date 함수 내부를 초기화 하는 것이고 add_day, add_month, add_year 는 일, 월, 년을 원하는 만큼 더하게 됩니다. 한 가지 주의할 점은 만일 2012 년 2 월 28 일에 3 일을 더하면 2012 년 2 월 31 일이 되는 것이 아니라 2012 년 3 월 2 일이 되겠지요? (난이도 : 上)
  */
 //이 코드는 그레고리력 달력을 참고했습니다.
+//"https://ko.wikipedia.org/wiki/윤년"
 #include <iostream>
 
 using namespace std;
 
-class Date {
+class Date
+{
+private:
+	
 	int year;
 	int month;
 	int day,dayMax;
 	bool LeapYear;
+	
 public:
 	void set_date(int _year, int _month, int _day)
 	{
@@ -26,25 +31,59 @@ public:
 		month = _month;
 		day = _day;
 	}
-	void isitLeapYear(int _year)//리턴값으로 윤년의 여부를 확인해주는 함수
+	void isitLeapYear()//리턴값으로 윤년의 여부를 확인해주는 함수
 	{
-		if(!(_year % 4))
+		LeapYear = 0;
+		if(!(year % 4))
 		{
 			LeapYear = 1;
 		}
-		if(!(_year % 100))
+		if(!(year % 100))
 		{
 			LeapYear = 0;
 		}
-		if(!(_year % 400))
+		if(!(year % 400))
 		{
 			LeapYear = 1;
+		}
+	}
+	void setDayMax()
+	{
+		switch (month)
+		{
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 11:
+				dayMax = 31;
+				break;
+				
+			case 4:
+			case 6:
+			case 9:
+				dayMax = 30;
+				break;
+				
+			case 2:
+				isitLeapYear();
+				if (LeapYear)
+				{
+					dayMax = 29;
+				}
+				else
+				{
+					dayMax = 28;
+				}
+			default:
+				break;
 		}
 	}
 	void add_day(int inc)
 	{
 		day += inc;
-		if(month)
 	}
 	void add_month(int inc)
 	{
@@ -67,11 +106,15 @@ int main(int argc, const char * argv[])
 	Date date;
 	
 	
-	for (; ; ) {
+	for (; ; )
+	{
 		cout << "1 : 날짜 설정\n2 : 추가\n3 : 날짜 표시\n";
 		cin >> index;
-		switch (index) {
+		
+		switch (index)
+		{
 			case 1:	//날짜 설정
+				
 				int year,month,day;
 				cout << "년 월 일 순으로 입력하세요.";
 				cin >> year >> month >> day;
@@ -79,6 +122,7 @@ int main(int argc, const char * argv[])
 				break;
 				
 			case 2:	//날짜 추가
+				
 				int select_add,inc;
 				cout << "1 : 일 추가\n2 : 월 추가\n3 : 년 추가\n";
 				cin >> select_add;
@@ -103,7 +147,9 @@ int main(int argc, const char * argv[])
 				break;
 				
 			case 3:	//날짜 표시
+				
 				date.get_date();
+				break;
 				
 			default:
 				break;
