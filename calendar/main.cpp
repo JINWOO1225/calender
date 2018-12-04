42dc3b50b09c5851ec2449c77fc0cc15acd0c874
JINWOO1225 <jinwoo17962@gmail.com>
2018-12-04 09:27:15 +0900

    Update main.cpp

--- calendar/main.cpp
+++ calendar/main.cpp
@@ -52,11 +52,11 @@ public:
 	{
 		switch (month)
 		{
-			case 1: case 3: case 5: case 7: case 8: case 10: case 11:
+			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
 				dayMax = 31;
 				break;
 				
-			case 4: case 6: case 9:
+			case 4: case 6: case 9: case 11:
 				dayMax = 30;
 				break;
 				
